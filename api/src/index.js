000beee01704018decd68a2fe7496b4956361b70
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-07-09 17:32:04 -0300

    reorganizando el servido

--- api/src/index.js
+++ api/src/index.js
@@ -9,7 +9,7 @@ console.log('connectio has been established successfully')
 app.listen(3000)
 console.log('server on port 3000')
 } catch (error){
-console.error('Unable to connect to the database', error)
+console.error('Unable to connect to the database', error);
 }
 }
 main()
