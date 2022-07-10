b8ef60db0163c55db699719597dda78ec9f3d670
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-07-10 13:26:24 -0300

    rutas terminadas con sus respectivas logicas

--- api/src/controllers/operations.controllers.js
+++ api/src/controllers/operations.controllers.js
@@ -2,12 +2,16 @@ import {operations} from '../models/operations.js'
 
 
 export const getOperations = async (req ,res) =>{
-const Operations = await operations.findAll();
-console.log(Operations)
-    res.send('operaciones')
+    try{
+        const Operations = await operations.findAll();
+        res.json(Operations)
+    }catch(error){
+        return res.status(500).json({message: error.message})
+    }
     
 }
 export const createOperations = async  (req ,res) =>{
+    try{
     const {concepto, fecha, monto, tipo} = req.body
     const newOperation = await operations.create({
         concepto,
@@ -15,7 +19,39 @@ export const createOperations = async  (req ,res) =>{
         monto,
         tipo,
     })
-    console.log(newOperation)
-    res.send('creatingOperaTions')
+    res.json(newOperation)
+    }catch(error){
+    return res.status(500).json({message: error.message})
+    }
+}
+
+export const updateOperations = async (req,res) =>{
+try{
+ const {id} = req.params
+const {concepto, monto, fecha, tipo } = req.body
+ const Operation = await operations.findByPk(id)
+ Operation.concepto = concepto;
+ Operation.monto = monto
+ Operation.fecha = fecha
+ Operation.tipo = tipo
+await Operation.save()
+res.json(Operation)
+} catch(error){
+return res.status(500).json({message: error.message})
+}
+}
+
+export const deleteOperations = async (req,res) =>{
+ try{
+    console.log(req.params.id)
+    const {id} = req.params
+    await operations.destroy({
+        where:{
+            id,
+        }})
+    res.sendStatus(204) 
+ } catch(error){
+ return res.status(500).json({message: error.message})
+ }
 }
 
--- api/src/routes/routes.operations.js
+++ api/src/routes/routes.operations.js
@@ -1,13 +1,13 @@
 import  {Router} from 'express';
-import {getOperations,  createOperations } from '../controllers/operations.controllers.js';
+import {getOperations,  createOperations, updateOperations, deleteOperations } from '../controllers/operations.controllers.js';
 
 
 const router = Router()
 
 router.get('/operations', getOperations)
 router.post('/operations',createOperations)
-router.put('/operations/:id')
-router.delete('/operations')
+router.put('/operations/:id',updateOperations )
+router.delete('/operations/:id', deleteOperations)
 router.get('/operations/:id')
 
 export default router
