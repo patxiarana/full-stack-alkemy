57475c2892e581732815d7caa6bb93779707165d
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-07-10 10:00:04 -0300

    creando user falta definir uso

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,24 +1,17 @@
-import {sequelize} from '../database/database.js'
-import {DataType, DataTypes} from 'sequelize'
+import {DataTypes} from 'sequelize';
+import {sequelize} from '../database/database.js';
 
 
-
-export const operations = sequelize.define('operations',{
-   id:{
-    type: DataTypes.INTEGER,
-    primarykey:true,
-    autoIncrement:true,
-   },
-  concepto:{
-    type:DataTypes.STRING
-  },
-  monto:{
-   types:DataTypes.REAL
-  },
-  fecha:{
-   type:DataTypes.DATEONLY
-  },
-  tipo:{
-    types:DataTypes.STRING
-  }
+export const user = sequelize.define('users',{
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true, 
+    },
+    email:{
+     type: DataTypes.STRING
+    },
+    password:{
+      type:DataTypes.INTEGER
+    },
 })
