cfa20040632a5f3e9e8eb5eb6290e3093590b0d2
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-07-10 09:59:00 -0300

    creando user falta definir uso

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,11 @@
 import app from './app.js'
 import  {sequelize}  from './database/database.js'
-
-
+import './models/operations.js'
+import './models/User.js'
 async function main(){
 await sequelize.authenticate()
 try {
+    await sequelize.sync();
 console.log('connectio has been established successfully')
 app.listen(3000)
 console.log('server on port 3000')
