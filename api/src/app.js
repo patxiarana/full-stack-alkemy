1e9a92906e6ff406651e848ac07a5ddf5b6ae427
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-07-13 09:39:49 -0300

    terminando de configurar cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,12 @@
 import express from "express"
 import operationsRoutes  from './routes/routes.operations.js'
-import cors from "cors"
+
 
 
 const app = express()
 
 //middlewars
 app.use(express.json())
-app.use(cors())
 
 // routes
 app.use(operationsRoutes)
--- api/src/routes/routes.operations.js
+++ api/src/routes/routes.operations.js
@@ -1,13 +1,28 @@
 import  {Router} from 'express';
 import {getOperations,  createOperations, updateOperations, deleteOperations, getToltal } from '../controllers/operations.controllers.js';
-
+import express from "express"
+//cors config
+import cors from "cors"
+// cors module
+const app = express()
+var whitelist =  ['http://localhost:3001']
+app.use(cors())
+var corsOptions = {
+    origin: function(origin,callback){
+     if(whitelist.indexOf(origin) != -1){
+        callback(null, true);
+         }else{
+            callback(new Error('not allowed by cors'));
+         }
+    }
+}
 
 const router = Router()
-router.get('/operations/total', getToltal)
-router.get('/operations', getOperations)
-router.post('/operations',createOperations)
-router.put('/operations/:id',updateOperations )
-router.delete('/operations/:id', deleteOperations)
+router.get('/operations/total',cors(corsOptions), getToltal)
+router.get('/operations',cors(corsOptions), getOperations)
+router.post('/operations',cors(corsOptions),createOperations)
+router.put('/operations/:id',cors(corsOptions),updateOperations )
+router.delete('/operations/:id',cors(corsOptions),deleteOperations)
 
 
 export default router
